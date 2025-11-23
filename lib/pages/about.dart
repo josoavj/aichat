8023ea848e7ae011c5cf3727284b45e83b21581f
Josoa Vonjiniaina <76913187+josoavj@users.noreply.github.com>
2025-11-23 20:59:20 +0300

    refactor: update firebase and settings pages
    Firebase Options:
    - Update configuration files
    
    Settings & About Pages:
    - Update to use new theme service
    - Improve UI consistency
    - Add support for async theme operations

--- lib/pages/about.dart
+++ lib/pages/about.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/gestures.dart'; // Import pour TapGestureRecognizer
+// Import pour TapGestureRecognizer
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:url_launcher/url_launcher.dart'; // Utilisation directe de url_launcher
